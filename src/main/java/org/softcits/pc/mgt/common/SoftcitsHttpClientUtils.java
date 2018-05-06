773942e153e29909a50caeb04861004e9f9b4af3
Jun <wangzhen_tju@126.com>
2018-05-06 15:37:12 +0800

    添加POST JSON远程调用方法

--- src/main/java/org/softcits/pc/mgt/common/SoftcitsHttpClientUtils.java
+++ src/main/java/org/softcits/pc/mgt/common/SoftcitsHttpClientUtils.java
@@ -5,40 +5,35 @@ import java.net.URI;
 import java.util.Map;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 public class SoftcitsHttpClientUtils {
 	
-	//不带参数的GET请求
 	public static String doGet(String url) {
 		return doGet(url, null);
 	}
 
-	//带参数的GET请求
 	public static String doGet(String url, Map<String, String> params) {
-		// 创建Httpclient对象
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 		String resultString = "";
 		CloseableHttpResponse response = null;
 		try {
-			// 创建uri
 			URIBuilder builder = new URIBuilder(url);
 			if (params != null) {
-				// 读取http url中的参数
 				for (String key : params.keySet()) {
 					builder.addParameter(key, params.get(key));
 				}
 
 			}
 			URI uri = builder.build();
-			// 创建http GET请求
 			HttpGet httpGet = new HttpGet(uri);
-			// 执行请求
 			response = httpclient.execute(httpGet);
-			// 判断返回状态是否为200
 			if (response.getStatusLine().getStatusCode() == 200) {
 				resultString = EntityUtils.toString(response.getEntity(), "UTF-8");
 			}
@@ -54,6 +49,35 @@ public class SoftcitsHttpClientUtils {
 				e.printStackTrace();
 			}
 		}
+		return resultString;
+	}
+	
+
+	public static String doPostJson(String url, String json) {
+		// 创建Httpclient对象
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		CloseableHttpResponse response = null;
+		String resultString = "";
+		try {
+			// 创建Http Post请求
+			HttpPost httpPost = new HttpPost(url);
+			// 创建请求内容
+			StringEntity entity = new StringEntity(json, ContentType.APPLICATION_JSON);
+			httpPost.setEntity(entity);
+			// 执行http请求
+			response = httpClient.execute(httpPost);
+			resultString = EntityUtils.toString(response.getEntity(), "utf-8");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				response.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
 		return resultString;
 	}
 
