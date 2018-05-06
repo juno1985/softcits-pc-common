84b5d2258622c6f1c1294ccf84ffd8347f106633
Jun <wangzhen_tju@126.com>
2018-05-06 10:31:13 +0800

    添加UUID工具类

---
+++ src/main/java/org/softcits/pc/mgt/common/UUIDUtil.java
@@ -0,0 +1,12 @@
+package org.softcits.pc.mgt.common;
+
+import java.util.UUID;
+
+public class UUIDUtil {
+	
+	public static String UUIDGenerator(){
+		String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+		return uuid;
+	}
+
+}
